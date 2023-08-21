c4cfa2850b70714feea501c3b8550a2877c43578
tykoon787 <tykoon787@protonmail.com>
2023-08-21 18:53:00 +0300

    Task done

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+This module defines a function that returns an obfuscated log message
+"""
+import re
+from typing import List
+
+
+def filter_datum(fields: List[str], redaction: List[str], message: str, separator: str) -> str:
+    """
+    This function will return a log message that is obfuscated 
+
+    Args:
+        fields __List[str]__: a list of strings representing all fields to obfuscate
+        redaction __List[str]__: a string representing by what the field will be obfuscated
+        message __str__: a string representing the log line
+        separator __str__: a string representing by which character is separating all fields in the log line (message)
+
+    Returns:
+        An obfuscated log msg
+    """
+    for field in fields:
+        message = re.sub(fr'{field}=[^;]+', f'{field}={redaction}', message)
+    return message
