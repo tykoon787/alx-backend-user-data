633861ca84c13dab3d928e2e7c21c5adafc59391
tykoon787 <tykoon787@protonmail.com>
2023-08-22 12:03:22 +0300

    updated py code

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,6 +3,7 @@
 This module defines a function that returns an obfuscated log message
 """
 import re
+import logging
 from typing import List
 
 
@@ -30,3 +31,21 @@ def filter_datum(fields: List[str], redaction: str, message: str,
         message = re.sub(fr'{field}=[^{separator}]+',
                          f'{field}={redaction}', message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """
+    Redacting Formatter Class
+    """
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ';'
+
+    def __init__(self, fields: List[str]):
+        self.fields = fields
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        message = super(RedactingFormatter, self).format(record)
+        return filter_datum(self.fields, self.REDACTION,
+                            message, self.SEPARATOR)
