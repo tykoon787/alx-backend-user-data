3f91ed9ce518d79882c20346b1a7b62ca1bb7ec8
tykoon787 <tykoon787@protonmail.com>
2023-08-22 12:27:49 +0300

    updated py code

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,7 +6,7 @@ import re
 import logging
 from typing import List
 
-PII_FIELDS = ("email", "name", "ssn", "password", "ip")
+PII_FIELDS = ("email", "name", "ssn", "password", "phone")
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
