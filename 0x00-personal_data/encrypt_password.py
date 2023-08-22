4c49f0b7b28c54c621bb1cc2b720207f9813aef5
tykoon787 <tykoon787@protonmail.com>
2023-08-22 13:00:35 +0300

    updated py code

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -8,3 +8,9 @@ def hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     hashed_pwd = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_pwd
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ Checks whether a pwd is valid """
+    hmm = bcrypt.checkpw(password.encode('utf-8'), hashed_password)
+    return hmm
