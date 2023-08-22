2e2be3d1e99f7787ca6c24ee21d075fa4c2c386f
tykoon787 <tykoon787@protonmail.com>
2023-08-22 14:19:06 +0300

    Task Done

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,7 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+@app_views.route('/api/v1/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized_endpoint():
+    abort(401)
